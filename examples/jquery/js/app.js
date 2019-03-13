28755fc2082c9b627717c922b5114348975acf31
RanDT1 <relidon@gmail.com>
2019-03-12 20:23:07 -0500

    Updated render method.
    Storing to local storage is now done in a separate function and called outside of render.

--- examples/jquery/js/app.js
+++ examples/jquery/js/app.js
@@ -52,6 +52,9 @@ jQuery(function ($) {
 				}.bind(this)
 			}).init('/all');
 		},
+		store: function() {
+			util.store('todos-jquery', this.todos);
+		},
 		bindEvents: function () {
 			$('#new-todo').on('keyup', this.create.bind(this));
 			$('#toggle-all').on('change', this.toggleAll.bind(this));
@@ -70,7 +73,6 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -90,7 +92,7 @@ jQuery(function ($) {
 			this.todos.forEach(function (todo) {
 				todo.completed = isChecked;
 			});
-
+      this.store();
 			this.render();
 		},
 		getActiveTodos: function () {
@@ -117,6 +119,7 @@ jQuery(function ($) {
 		destroyCompleted: function () {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
+			this.store();
 			this.render();
 		},
 		// accepts an element from inside the `.item` div and
@@ -147,12 +150,13 @@ jQuery(function ($) {
 			});
 
 			$input.val('');
-
+      this.store();
 			this.render();
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
+			this.store();
 			this.render();
 		},
 		edit: function (e) {
@@ -183,11 +187,12 @@ jQuery(function ($) {
 			} else {
 				this.todos[this.indexFromEl(el)].title = val;
 			}
-
+      this.store();
 			this.render();
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
+			this.store();
 			this.render();
 		}
 	};
